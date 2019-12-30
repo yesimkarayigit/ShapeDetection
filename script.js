7d01a1c7e92d9af64a79cbb2de52ec2e3cf1f346
yesim <yesim.karayigit@gmail.com>
2019-12-31 00:24:02 +0300

    Detect face and landmarks and mark them on a live video

--- script.js
+++ script.js
@@ -1,63 +1,65 @@
-const image = document.getElementById('image');
-const button = document.querySelector('button');
-const section = document.querySelector('section');
-
-const video = document.querySelector('.video-player');
+/* const video = document.querySelector('.video-player'); */
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-
-function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-  .then(localMediaStream => {
-    video.srcObject = localMediaStream;
-    video.play();
-  })
-  .catch(err => {
-    console.error(`OH NO!!!`, err);
-  });
-}
-
-function detect() {
-
-  const width = video.videoWidth;
-  const height = video.videoHeight;
-  canvas.width = width;
-  canvas.height = height;
-
-  context.clearRect(0, 0, width, height);
-  context.drawImage(video, 0, 0, width, height);
+const video = document.createElement('video');
+const constraints = { video: true, audio: false };
+const mediaStream =  navigator.mediaDevices.getUserMedia(constraints)
+.then(localMediaStream => {
+  video.srcObject = localMediaStream;
+  video.autoplay = true;
+  video.onloadedmetadata = () => {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+  };
+})
+.catch(err => {
+  console.error(`OH NO!!!`, err);
+});
 
 
-  context.strokeStyle = '#ffeb3b';
-  context.fillStyle = '#ffeb3b';
-  context.font = '16px Mononoki';
-  context.lineWidth = 5;
-  
-  console.log('yeap!');
-  var faceDetector = new FaceDetector();
+
+function detectFace() {
+
+  const faceDetector = new FaceDetector({fastMode: true});
 
   faceDetector.detect(video)
-    .then(faces => faces.forEach(face => {     
+    .then((faces) => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    
+      context.strokeStyle = '#ffeb3b';
+      context.fillStyle = '#ffeb3b';
+      context.font = '30px Mononoki';
+      context.lineWidth = 5;
+      
+      faces.forEach(face => {     
       const { top, left, width, height } = face.boundingBox;
       context.beginPath();
       context.rect(left, top, width, height);
       context.stroke();
-      context.fillText('face detected', left + 5, top - 8);
+      context.fillText('face', left + 1, top - 6);
       
       if(face.landmarks) {
-        face.landmarks.forEach( () => {
-          let arrayFromLandmarks = face.landmarks;
-          section.innerHTML = arrayFromLandmarks.map(landmark => {
-              return `<p>${landmark.type}</p>`;
-          }).join('')
+        face.landmarks.forEach( (landmark) => {
+          if(landmark.type === 'eye') {
+            context.beginPath();
+            context.fill();
+            context.fillText("🌸", landmark.locations[0].x, landmark.locations[0].y);
+          } else {
+            context.beginPath();
+            context.fill();
+            context.fillText("🔥", landmark.locations[0].x, landmark.locations[0].y);
+          }
         }) 
       }
-    }))
+    })
+  })
     .catch((e) => {
       console.error('ops!');
     })
+    requestAnimationFrame(detectFace);
 }
 
-getVideo();
-button.addEventListener('click', detect);
+window.requestAnimationFrame(detectFace);
+video.addEventListener('play', detectFace); 
